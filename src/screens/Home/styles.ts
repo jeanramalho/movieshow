463242b35c0241316276864138c268daa7895ca4
jeanramalho <jeanramalho.dev@gmail.com>
2023-04-17 19:07:30 -0300

    Começando estilizações

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,9 +4,15 @@ import {  StyleSheet  } from 'react-native'
  export const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      backgroundColor: '#1e1e1e',
+      padding: 25,
+    },
+
+    headerText: {
+      marginTop: 30,
+      fontSize: 24,
+      lineHeight: 45,
+      color: "#FFF",
     },
   });
   
